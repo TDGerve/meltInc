9f92da00e8f5a294d2d9eaa35bea2d6753cd3df7
TDGerve <thomasvangerve@gmail.com>
2022-02-08 09:44:04 +0100

    update

--- petroPy/plotStyle/plotStyle.py
+++ petroPy/plotStyle/plotStyle.py
@@ -1,20 +1,33 @@
 import matplotlib.pyplot as plt
     
-# Colour palettes
-colors = {
-    'flatDesign': plt.cycler(color= ['#e27a3d', '#344d5c', '#df5a49', '#43b29d', '#efc94d']), 
-    'firenze': plt.cycler(color= ['#8E2800','#468966', '#B64926','#FFF0A5', '#FFB03B']),
-    'vitaminC': plt.cycler(color= ['#FD7400','#004358', '#FFE11A', '#1F8A70', '#BEDB39']),
-    'bella': plt.cycler(color= ['#801637', '#047878', '#FFB733', '#F57336', '#C22121']),
-    'buddha': plt.cycler(color= ['#192B33', '#FF8000', '#8FB359', '#FFD933', '#CCCC52']),
-    'elemental': plt.cycler(color= ['#E64661', '#FFA644', '#998A2F', '#2C594F', '#002D40']),
-    'carolina': plt.cycler(color= ['#73839C', '#2E4569', '#AECCCF', '#D5957D', '#9C7873']),
-    '42': plt.cycler(color= ['#2469A6', '#C4E1F2', '#F2E205', '#F2D22E', '#D9653B']),
-    'terrazaverde': plt.cycler(color= ['#DFE2F2', '#88ABF2', '#4384D9', '#56BFAC', '#D9B341'])
-    }
+# Color palettes
+class colors:
+    """
+    Color palettes for plots
+    """
+    
+     
+    flatDesign =  plt.cycler(color= ['#e27a3d', '#344d5c', '#df5a49', '#43b29d', '#efc94d'])
+
+    firenze =  plt.cycler(color= ['#8E2800','#468966', '#B64926','#FFF0A5', '#FFB03B'])
+
+    vitaminC =  plt.cycler(color= ['#FD7400','#004358', '#FFE11A', '#1F8A70', '#BEDB39'])
+
+    bella =  plt.cycler(color= ['#801637', '#047878', '#FFB733', '#F57336', '#C22121'])
+
+    buddha =  plt.cycler(color= ['#192B33', '#FF8000', '#8FB359', '#FFD933', '#CCCC52'])
+
+    elemental =  plt.cycler(color= ['#E64661', '#FFA644', '#998A2F', '#2C594F', '#002D40'])
+
+    carolina =  plt.cycler(color= ['#73839C', '#2E4569', '#AECCCF', '#D5957D', '#9C7873'])
+
+    fourtyTwo =  plt.cycler(color= ['#2469A6', '#C4E1F2', '#F2E205', '#F2D22E', '#D9653B'])
+
+    terrazaverde =  plt.cycler(color= ['#DFE2F2', '#88ABF2', '#4384D9', '#56BFAC', '#D9B341'])
+    
 
 
-def layout(fontSize= 16, axTitleSize= 16, axLabelSize= 16, tickLabelSize= 12, legendFontSize= 8, colors= colors['firenze']):
+def layout(fontSize= 16, axTitleSize= 16, axLabelSize= 16, tickLabelSize= 12, legendFontSize= 8, colors= colors.firenze):
 
     plt.rcParams['figure.constrained_layout.use'] = True
     plt.rcParams['figure.figsize'] = (8, 7)
